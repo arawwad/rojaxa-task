d84a40f6f050b885e547113c08a7fb45c38632a1
arawwad <ar.awwad6@gmail.com>
2017-11-06 22:58:23 +0200

    basic layout

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      :mini-variant.sync="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed app :clipped-left="clipped">
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <main>
+      <v-content>
+        <v-container fluid>
+        </v-container>
+      </v-content>
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: true,
+        drawer: true,
+        fixed: false,
+        items: [{
+          icon: 'bubble_chart',
+          title: 'Inspire',
+        }],
+        miniVariant: true,
+        right: null,
+        rightDrawer: false,
+        title: 'Logo',
+      };
+    },
+  };
+
+</script>
